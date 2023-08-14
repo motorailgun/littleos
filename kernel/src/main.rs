82fe6ac030005c34a277c4fa3ae199c0a3e3108e
motorailgun <28751910+motorailgun@users.noreply.github.com>
2023-08-14 19:44:10 +0900

    serial: fix visibility

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -4,7 +4,6 @@
 mod serial;
 
 use core::panic::PanicInfo;
-use serial::SerialPort;
 
 #[panic_handler]
 pub fn panic(_info: &PanicInfo) -> ! {
@@ -15,7 +14,7 @@ static HELLO: &[u8] = b"\n\n\tHello World!\n\n";
 const SERIAL_PORT_ADDRESS: u16 = 0x3f8;
 
 fn kernel_entry(_boot_info: &'static mut bootloader_api::BootInfo) -> ! {
-    let serial_port = SerialPort::new(SERIAL_PORT_ADDRESS).unwrap();
+    let serial_port = serial::new(SERIAL_PORT_ADDRESS).unwrap();
     serial_port.write_bytes(HELLO);
 
     loop {}
--- kernel/src/serial.rs
+++ kernel/src/serial.rs
@@ -7,36 +7,36 @@ pub struct SerialPort {
 static init_complete_message: &[u8] = b"Serial port initialized!\n";
 const SERIAL_PORT_RATE: u8 = (115200u32 / 9600) as u8;
 
-impl SerialPort {
-    // serial port setup, basically based on OSDev wiki:
-    // lookup: https://wiki.osdev.org/Serial_Ports
-    pub fn new(port: u16) -> Option<SerialPort> {
-        unsafe {
-            outb(port + 1, 0x00); // Disable all interrupts
-            outb(port + 3, 0x80); // Enable DLAB (set baud rate divisor)
-            outb(port + 0, SERIAL_PORT_RATE); // Set divisor to 12 (lo byte) 115200 baud
-            outb(port + 1, 0x00); //                   (hi byte)
-            outb(port + 3, 0x03); // 8 bits, no parity, one stop bit
-            outb(port + 2, 0xC7); // Enable FIFO, clear them, with 14-byte threshold
-            outb(port + 4, 0x0B); // IRQs enabled, RTS/DSR set
-            outb(port + 4, 0x1E); // Set in loopback mode, test the serial chip
-            outb(port + 0, 0xAE); // Test serial chip (send byte 0xAE and check if serial returns same byte)
+// serial port setup, basically based on OSDev wiki:
+// lookup: https://wiki.osdev.org/Serial_Ports
+pub fn new(port: u16) -> Option<SerialPort> {
+    unsafe {
+        outb(port + 1, 0x00); // Disable all interrupts
+        outb(port + 3, 0x80); // Enable DLAB (set baud rate divisor)
+        outb(port + 0, SERIAL_PORT_RATE); // Set divisor to 12 (lo byte) 115200 baud
+        outb(port + 1, 0x00); //                   (hi byte)
+        outb(port + 3, 0x03); // 8 bits, no parity, one stop bit
+        outb(port + 2, 0xC7); // Enable FIFO, clear them, with 14-byte threshold
+        outb(port + 4, 0x0B); // IRQs enabled, RTS/DSR set
+        outb(port + 4, 0x1E); // Set in loopback mode, test the serial chip
+        outb(port + 0, 0xAE); // Test serial chip (send byte 0xAE and check if serial returns same byte)
 
-            if inb(port + 0) != 0xAE {
-                // check if the serial port is faulty
-                return None;
-            }
-
-            // not-loopback with IRQs enabled and OUT#1 and OUT#2 bits enabled
-            outb(port + 4, 0x0F);
+        if inb(port + 0) != 0xAE {
+            // check if the serial port is faulty
+            return None;
         }
 
-        let sp = SerialPort { port };
-        sp.write_bytes(init_complete_message);
-
-        Some(sp)
+        // not-loopback with IRQs enabled and OUT#1 and OUT#2 bits enabled
+        outb(port + 4, 0x0F);
     }
 
+    let sp = SerialPort { port };
+    sp.write_bytes(init_complete_message);
+
+    Some(sp)
+}
+
+impl SerialPort {
     fn is_transmit_empty(&self) -> bool {
         (unsafe { inb(self.port + 5) & 0x20 } > 0)
     }
